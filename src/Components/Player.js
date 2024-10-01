cb34e1bb85d0255549f9e9af231c69ce72d39a0e
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-10-01 19:55:27 +0300

    79. User Input & Two-Way-Binding

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,11 +1,30 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initName);
+
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChangeName(event) {
+    setPlayerName(event.target.value)
+  }
+
+  let playerNameField = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerNameField = <input type="text" required value={playerName} onChange={handleChangeName}></input>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerNameField}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditing}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
