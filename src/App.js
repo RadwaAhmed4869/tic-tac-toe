ba17105fb6bef94bf5b822c172496fe289592b83
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-10-03 02:18:40 +0300

    87. Reducing State Management & Identifying Unnecessary State

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import Player from "./Components/Player";
 import GameBoard from "./Components/GameBoard";
 import Log from "./Components/Log";
 
+function getActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = getActivePlayer(gameTurns);
 
   function handleActivePlayer(rowIndex, colIndex) {
-    setActivePlayer((preActivePlayer) => (preActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((preActivePlayer) => (preActivePlayer === "X" ? "O" : "X"));
 
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = getActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -44,7 +51,7 @@ function App() {
         </ol>
         <GameBoard onClickSquare={handleActivePlayer} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
